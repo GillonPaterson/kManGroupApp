b3ae359f04574a3a497b028e9f91fc7bdf3e1ec9
Gareth Miskimmin <92785721+garethmiskimmin@users.noreply.github.com>
2021-12-13 12:41:46 +0000

    Revert "UI"

--- app/jobrolesservice.js
+++
@@ -1,9 +0,0 @@
-const NodeCache = require("node-cache");
-const myCache = new NodeCache();
-const axios = require('axios').default;
-
-// User Story 1
-exports.getJobRoles = async() => {
-    const response = await axios.get('http://localhost:8080/api/getJobRoles');
-    return response.data;
-}
