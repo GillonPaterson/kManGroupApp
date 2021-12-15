04cbdfe197400028cb56f99f97014fb705f9689f
Nikkala Pokojski <nikkala.pokojski@kainos.com>
2021-12-15 09:06:58 +0000

    updating

--- app/routes.js
+++ app/routes.js
@@ -11,6 +11,7 @@ router.get("/jobroles", async(req, res) => {
     var role =  await jobrolesservice.getJobRoles()
     console.log(role)
     for(i = 0; i < role.length; i++){
+        role[i].jobBandLevel = "<a href=http://localhost:3000/"+role[i].jobBandLevel.toLowerCase()+"competencies.html> "+role[i].jobBandLevel+"</a>"
         role[i].viewSpecURL = "<a href=http://localhost:3000/jobSpec?jobRoleID="+role[i].jobRoleID+">More Info</a>"
     }
     res.render('jobroles.html', { jobroles: role })
--- test/jobroleservicetest.js
+++ test/jobroleservicetest.js
@@ -25,7 +25,19 @@ describe("Job Role Service", function() {
         var result = await employeeservice.getJobRoles();
 
         expect(result.length).to.equal(2);
-        expect(list[0]).to.equal(jobrole1);
-        expect(list[1]).to.equal(jobrole2);
+        expect(result[0].val).to.equal(jobrole1.val);
+        expect(result[1].val).to.equal(jobrole2.val);
+    });
+
+    it("Should return a info about a jobRole", async() => {
+        var mock = new MockAdapter(axios);
+        var jobrole = { jobRoleID: 1 , jobRole: "Software Engineer"};
+
+
+        mock.onGet('http://localhost:8080/api/getJobSpec/1').reply(200, jobrole);
+
+        var result = await employeeservice.getJobRoleSpec(1)
+
+        expect(result).to.eql(jobrole);
     });
 });
