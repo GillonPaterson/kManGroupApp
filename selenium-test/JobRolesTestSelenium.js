0f5d045c6685faba9affb5d03b2edc69bda21802
Gareth Miskimmin <gareth.miskimmin@kainos.com>
2021-12-15 12:37:03 +0000

    selenium test check first row

--- selenium-test/JobRolesTestSelenium.js
+++ selenium-test/JobRolesTestSelenium.js
@@ -6,11 +6,20 @@ require('chromedriver');
 describe("Selenium test", () => {
     const driver = new Builder().forBrowser('chrome').build();
 
-    it('should wait until title is right', async () => {
+    it('should have correct first row displayed on job roles page', async () => {
       await driver.get('http://localhost:3000/jobroles');
+
+      expectedFirstRow = [ '1', 'Software Engineer', 'Engineering' ];
+
       const title = await driver.getTitle();
+      var elements = (await driver.findElements(By.className("govuk-table__cell")))
+      var id = await elements[0].getText()
+      var jobRole = await elements[1].getText()
+      var jobCapability = await elements[2].getText()
+      var firstRow = [id,jobRole,jobCapability]
 
       expect(title).to.equal("List of Job Roles");
+      expect(firstRow).to.eql(expectedFirstRow);
     });
 
     after(async () => driver.quit());
