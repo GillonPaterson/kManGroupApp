b4619ac94804a9ab11730ad4bbe955721d6899ad
Nikkala Pokojski <nikkala.pokojski@kainos.com>
2021-12-15 10:49:39 +0000

    links

--- app/routes.js
+++ app/routes.js
@@ -7,9 +7,12 @@ const NodeCache = require("node-cache");
 const myCache = new NodeCache();
 
 
+router.get("/home", async(req, res) =>{
+    res.render('home.html')
+});
+
 router.get("/jobroles", async(req, res) => { 
     var role =  await jobrolesservice.getJobRoles()
-    console.log(role)
     for(i = 0; i < role.length; i++){
         role[i].jobBandLevel = "<a href=http://localhost:3000/"+role[i].jobBandLevel.toLowerCase()+"competencies.html> "+role[i].jobBandLevel+"</a>"
         role[i].viewSpecURL = "<a href=http://localhost:3000/jobSpec?jobRoleID="+role[i].jobRoleID+">More Info</a>"
@@ -19,7 +22,6 @@ router.get("/jobroles", async(req, res) => {
 
 router.get("/jobSpec", async(req, res) =>{
     var role = await jobrolesservice.getJobRoleSpec(req.query.jobRoleID)
-    console.log(role[0].jobRole)
     res.render('jobSpec.html', {
         jobRoleInfo: role[0]
     })
