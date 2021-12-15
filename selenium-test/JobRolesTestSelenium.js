b54011e673e51eca1e5d3971abed5994ea7864c4
Gareth Miskimmin <gareth.miskimmin@kainos.com>
2021-12-15 13:45:40 +0000

    tests

--- selenium-test/JobRolesTestSelenium.js
+++ selenium-test/JobRolesTestSelenium.js
@@ -6,20 +6,75 @@ require('chromedriver');
 describe("Selenium test", () => {
     const driver = new Builder().forBrowser('chrome').build();
 
+    it('should wait until home page opens', async () => {
+      await driver.get('http://localhost:3000/home');
+      const title = await driver.getTitle();
+      const button = await driver.findElement(By.xpath('//*[@id="main-content"]/div/div[1]/a')).getText();
+
+      expect(title).to.equal("Job Roles");
+      expect(button).to.equal("Search jobs");
+    });
+
+    it('should click button and open job roles', async () => {
+      driver.findElement(By.xpath('//*[@id="main-content"]/div/div[1]/a')).click();
+      await driver.wait(until.titleIs("List of Job Roles"));
+
+      const title = await driver.getTitle();
+
+      expect(title).to.equal("List of Job Roles");
+    });
+  
     it('should have correct first row displayed on job roles page', async () => {
-      await driver.get('http://localhost:3000/jobroles');
 
       expectedFirstRow = [ '1', 'Software Engineer', 'Engineering' ];
 
-      const title = await driver.getTitle();
       var elements = (await driver.findElements(By.className("govuk-table__cell")))
       var id = await elements[0].getText()
       var jobRole = await elements[1].getText()
       var jobCapability = await elements[2].getText()
       var firstRow = [id,jobRole,jobCapability]
+      expect(firstRow).to.eql(expectedFirstRow);
+    });
+
+    it('should click more info and display job spec', async () => {
+      driver.findElement(By.linkText('More Info')).click();
+      await driver.wait(until.titleIs("Job Specification"));
+      const title = await driver.getTitle();
+
+      expect(title).to.equal("Job Specification");
+    });
+
+    it('should click back and display job roles', async () => {
+      driver.findElement(By.linkText('Back')).click();
+      await driver.wait(until.titleIs("List of Job Roles"));
+      const title = await driver.getTitle();
 
       expect(title).to.equal("List of Job Roles");
-      expect(firstRow).to.eql(expectedFirstRow);
+    });
+
+    it('should click band level and display band level', async () => {
+      driver.findElement(By.linkText('Associate')).click();
+      await driver.wait(until.titleIs("Associate Competencies"));
+      const title = await driver.getTitle();
+
+      expect(title).to.equal("Associate Competencies");
+    });
+
+    it('should click back and display job roles', async () => {
+      driver.findElement(By.linkText('Back')).click();
+      await driver.wait(until.titleIs("List of Job Roles"));
+
+      const title = await driver.getTitle();
+
+      expect(title).to.equal("List of Job Roles");
+    });
+
+    it('should click home and display home', async () => {
+      driver.findElement(By.linkText('Home')).click();
+      await driver.wait(until.titleIs("Job Roles"));
+      const title = await driver.getTitle();
+
+      expect(title).to.equal("Job Roles");
     });
 
     after(async () => driver.quit());
