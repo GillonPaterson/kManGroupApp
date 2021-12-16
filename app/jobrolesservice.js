1d1651f458d0efd97d5b6940180fb781991f4a44
GillonPaterson <gillon.paterson@kainos.com>
2021-12-16 15:53:38 +0000

    USC-005-made-dynamic

--- app/jobrolesservice.js
+++ app/jobrolesservice.js
@@ -22,7 +22,13 @@ exports.getJobRoleSpec = async(jobRoleID) =>{
     }
 }
 
-exports.getCompetencyData = async(bandLevel,jobRoleID) =>{
-    const response = await axios.get('http://localhost:8080/api/getJobCompetency/'+bandLevel+jobRoleID)
-    return response.data;
+exports.getCompetencyData = async(jobRoleID) =>{
+    try{
+        const response = await axios.get('http://localhost:8080/api/getJobCompetency/'+jobRoleID)
+        return response.data;
+    }catch(e)
+    {
+        return;
+    }
+
 }
