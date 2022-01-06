3284b95af9dfc2f361b6d8bf066ac48c1f2bff3c
GillonPaterson <gillon.paterson@kainos.com>
2022-01-06 10:37:58 +0000

    USC-013-validation

--- app/validator/capabilityValidator.js
+++ app/validator/capabilityValidator.js
@@ -1,14 +1,21 @@
 exports.checkCapability = async(capability) =>{
     capName = capability.capabilityName
     console.log(capName)
-    error = false
+    error = "no error"
+
+    const regex = /\d/;
 
     if (capName.length > 20 || capName.length == 0) {
-        console.log("forename")
-        error = true
-    } else{
-        error = false;
-
+        console.log("capability")
+        error = "Capability Name can only be 20 characters long"
+    } else if ((regex.test(capName)) == true){
+        error = "Capability Name must not contain numbers"
+    }else if (capName.charAt(0) == " "){
+        error = "Capability Name must not begin with a space"
+    }else if (capName.charAt(capName.length-1) == " "){
+        error = "Capability Name must not end with a space"
+    }else{
+        error = "no error";
     }   
 
     return error;
