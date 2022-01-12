54e50df358aaaa699a84dd219a5bc3939c358d6f
Gareth Miskimmin <gareth.miskimmin@kainos.com>
2022-01-12 15:10:29 +0000

    epic

--- app/services/loginService.js
+++ app/services/loginService.js
@@ -1,18 +1,24 @@
 const axios = require('axios').default;
-const auth = require("../lib/middleware/authentication/authoriser.js");
+const auth = require("../../lib/middleware/authentication/authoriser.js");
 
 exports.login = async(loginInfo) =>{
+    
     try{
         const response = await axios.post('http://localhost:8080/api/login', loginInfo)
         var token = response.headers.authorization.split(" ")[1]
 
-        const secret = Buffer.from("2w0lavt3CFAAqAY1z4q+LpZfCNW5gLH+udmMfi/Tl6g=", 'base64')
-        const jwt = require('jsonwebtoken');
-        jwt.verify(token,secret)
+        if(token == null){
+            return false;
+        }
+        else{
+            const secret = Buffer.from("2w0lavt3CFAAqAY1z4q+LpZfCNW5gLH+udmMfi/Tl6g=", 'base64')
+            const jwt = require('jsonwebtoken');
+            jwt.verify(token,secret)
 
-        return token
+            return token
+        }
     }catch(e){
-        console.log(e)
+        // console.log(e)
         return false;
     }
 }
